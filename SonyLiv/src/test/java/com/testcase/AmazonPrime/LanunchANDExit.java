b493c6150bf4e3b5f8b720208fb209ecf90d9673
insum <insum@LAPTOP-8EO5ID8Q>
2019-07-10 11:57:06 +0530

    added testcase

--- SonyLiv/src/test/java/com/testcase/AmazonPrime/LanunchANDExit.java
+++ SonyLiv/src/test/java/com/testcase/AmazonPrime/LanunchANDExit.java
@@ -32,42 +32,7 @@ public class LanunchANDExit extends Base{
 		navi.Movies(driver);
 		navi.Kids(driver);
 		
-		
-		
-		
-//		driver.findElementByAndroidUIAutomator(
-//				"new UiScrollable(new UiSelector().resourceId(\"com.amazon.avod.thirdpartyclient:id/LandingPageScrollView\")).scrollIntoView(new UiSelector().text(\"Lifestyle\"));");
-//		System.out.println("Lifestyle found");
-//		Thread.sleep(5000);
-		
-//		driver.findElement(By.xpath("//*[@text='TV Shows']")).click();
-//		System.out.println("Moved to TV Shows Section");
-//		Thread.sleep(2000);
-//		
-//		driver.findElementByAndroidUIAutomator(
-//				"new UiScrollable(new UiSelector().resourceId(\"com.amazon.avod.thirdpartyclient:id/LandingPageScrollView\")).scrollIntoView(new UiSelector().text(\"Anime TV\"));");
-//		System.out.println("Anime TV found");
-//		Thread.sleep(5000);
-		
-//		driver.findElement(By.xpath("//*[@text='Movies']")).click();
-//		Thread.sleep(2000);
-//		
-//		driver.findElementByAndroidUIAutomator(
-//				"new UiScrollable(new UiSelector().resourceId(\"com.amazon.avod.thirdpartyclient:id/LandingPageScrollView\")).scrollIntoView(new UiSelector().text(\"Movies in Gujarati \"));");
-//		System.out.println(" Movies in Gujarati found");
-//		Thread.sleep(5000);
-		
-		
-		
-//		driver.findElement(By.xpath("//*[@text='Kids']")).click();
-//		Thread.sleep(2000);
-//		System.out.println("Moved To Kids section");
-//		
-//		driver.findElementByAndroidUIAutomator(
-//				"new UiScrollable(new UiSelector().resourceId(\"com.amazon.avod.thirdpartyclient:id/LandingPageScrollView\")).scrollIntoView(new UiSelector().text(\"Movies and TV shows with animals\"));");
-//		System.out.println("Movies and TV shows with animals found");
-//		Thread.sleep(5000);
-		
+	
 		driver.pressKeyCode(AndroidKeyCode.BACK);
 		System.out.println("App Exits");
 		
